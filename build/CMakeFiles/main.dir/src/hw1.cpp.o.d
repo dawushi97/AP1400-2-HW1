ed7affe6f74df97245a0aa4b20b62e21d1e5429e
dawushi97 <dawushi97@gmail.com>
2025-01-05 19:11:18 +0800

    clean up repository with .gitignore

--- build/CMakeFiles/main.dir/src/hw1.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/main.dir/src/hw1.cpp.o: \
-  /Users/zhangyishun/Code/LearnProject/AP1400-2/AP1400-2-HW1/src/hw1.cpp \
-  /Users/zhangyishun/Code/LearnProject/AP1400-2/AP1400-2-HW1/include/hw1.h
